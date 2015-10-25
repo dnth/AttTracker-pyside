6d1f02232d9876a65ea84c0b74c14b9c7279ff32
dnth <dickson.neoh@gmail.com>
2015-10-26 02:35:37 +0800

    added pyinstaller executable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = ['collections.abc'], include_files = ['pics/', 'python.jpg', 'unknown_profile.png'] )
+buildOptions = dict(packages = ["PySide.QtCore", "PySide.QtGui", "PySide.QtXml"], includes = ["atexit"], excludes = ['collections.abc'], include_files = ['pics/', 'python.jpg', 'unknown_profile.png'] )
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
@@ -13,8 +13,8 @@ executables = [
 
 setup(
     name='AttTracker',
-    version = '0.1',
-    description = 'A PyQt Attendance Program',
+    version = '0.1.1',
+    description = 'A PySide Attendance Program',
     options = dict(build_exe = buildOptions),
     executables = executables
 )
